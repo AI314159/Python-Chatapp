16f552892f4d597121cfcfafbbc5c64bb89cdbf4
AI314159 <104926797+AI314159@users.noreply.github.com>
2024-12-22 12:28:57 -0500

    Added a user registration page.

--- chatapp/user/views.py
+++ chatapp/user/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+
+def register_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        confirm_password = request.POST["confirm_password"]
+
+        if password != confirm_password:
+            context = {"error_message": "Passwords did not match."}
+            return render(request, "register.html", context)
+        user = User(username=username, password=password)
+        user.save()
+        login(request, user)
+        return redirect("/")
+    else:
+        return render(request, "register.html")
 
 def login_user(request):
     if request.method == "POST":
